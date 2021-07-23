3d6f3e4994869774c6edfd922bca4da961a11d02
Tony Lu <70731161+tlu8592@users.noreply.github.com>
2021-07-23 05:50:32 -0400

    curly braces implemented

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
+  const image =
+    "https://i.pinimg.com/originals/92/94/ba/9294badee7b8f3d93fa9bc6c874641b2.png";
+  const titleObj = {
+    title: "Light Bulb!"
+  };
   return (
     <div className="App">
-      <img
-        src="https://i.pinimg.com/originals/92/94/ba/9294badee7b8f3d93fa9bc6c874641b2.png"
-        alt="lightbulb"
-      />
+      <h2>{titleObj.title}</h2>
+      <img src={image} alt="lightbulb" />
     </div>
   );
 }
